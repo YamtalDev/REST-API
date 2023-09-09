475723ddbc82d363e1d7e6c5cc4f46a8bd87be77
YamtalDev <tal.aharon.work@gmail.com>
2023-09-09 22:51:07 +0300

    Updating

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,20 @@
 import config from 'config';
 import routes from './routes';
 import express from 'express';
-import log from './utils/logger';
+import logger from './utils/logger';
 import connect from './utils/connect';
 import deserializeUser from './middleware/deserializeUser';
 
 const port = config.get<number>("port");
-const host = config.get("host") as string;
 
 const app = express();
 app.use(express.json());
 app.use(deserializeUser);
 app.use(express.urlencoded({extended: false}));
 
-app.listen(port, host, async () =>
+app.listen(port, async () =>
 {
-    log.info(`Server running at http://${host}:${port}`);
+    logger.info(`Server running at http://localhost:${port}`);
     await connect();
     routes(app);
 });
